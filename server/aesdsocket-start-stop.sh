1887d67696030b1972682c352c5f3855a60a7fe6
Amogh <amoghsadhu27@gmail.com>
2026-03-20 14:38:26 +0530

    Updated the starter script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,6 +2,10 @@
 
     case $1 in
         start)
+	    echo "Loading aesdchar driver"
+      	    # Load the driver first to create /dev/aesdchar
+	    aesdchar_load
+
             echo "Starting aesdsocket"
             start-stop-daemon -S -n aesdsocket  -a /usr/bin/aesdsocket -- -d
             ;;
@@ -9,6 +13,10 @@
         stop)
             echo "Stopping aesdsocket"
             start-stop-daemon -K -n aesdsocket
+	    
+	    echo "Unloading aesdchar driver"
+            # Unload the driver to clean up the kernel module and /dev node
+            aesdchar_unload
             ;;
         *)
             echo "Usage : $0 {start | stop}"
